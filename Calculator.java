ed20ea338a4b8ff5daf8d64a1b6088c16a901ebb
Md. Nahid Hassan <43172123+nahiddothub@users.noreply.github.com>
2022-06-02 00:01:11 +0600

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,20 @@
+package infinity;
+
+public class Calculator {
+    public Calculator() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Addition of two values;");
+        System.out.print("2 + 5 = ");
+        System.out.println(7);
+        System.out.println("---------");
+        System.out.println("2 + 5 = 7");
+        System.out.println("subtraction of two values;");
+        System.out.println("2 - 3 = -1");
+        System.out.println("Multiply of two values;");
+        System.out.println("2 * 3 =6");
+        System.out.println("Division of two values;");
+        System.out.println("4 / 2 = 2");
+    }
+}
